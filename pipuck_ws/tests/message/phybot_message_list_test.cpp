50fa256ab71a8bdccfe156806ca10da17934a792
ReemZohar <thefirstreem@gmail.com>
2026-08-10 18:14:29 +0300

    refactor: split message list tests into smaller mini tests

--- pipuck_ws/tests/message/phybot_message_list_test.cpp
+++ pipuck_ws/tests/message/phybot_message_list_test.cpp
@@ -16,18 +16,39 @@ void initMsg(CPhybotMessage& msg, uint32_t timestamp) {
     msg.m_unTimestamp = timestamp;
 }
 
-// Tests that messages are added successfully to the message list.
-TEST(PhybotMessageListTest, MessageAddition) {
-    // Empty list test
+// Test that a newly created message list is empty.
+TEST(PhybotMessageListTest, StartsEmpty) {
     CPhybotMessageList emptyList;
 
     // Both lists should be empty
     EXPECT_EQ(emptyList.getIncomingMessages().size(), 0);
     EXPECT_EQ(emptyList.getOutgoingMessages().size(), 0);
+}
 
+// Tests that 1 message is added successfully to each message list.
+TEST(PhybotMessageListTest, AddsOneMessagePerList) {
     CPhybotMessageList list;
-    size_t expectedInSize1 = 1, expectedOutSize1 = 1;
-    size_t expectedInSize2 = 4, expectedOutSize2 = 2;
+    size_t expectedInSize = 1, expectedOutSize = 1;
+
+    // Random timestamp values for the messages
+    uint32_t t1 = 1, t2 = 19;
+
+    std::unique_ptr<CPhybotMessage> incMsg1 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*incMsg1, t1);
+    std::unique_ptr<CPhybotMessage> outMsg1 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*outMsg1, t2);
+
+    list.addMessage(std::move(incMsg1), true);
+    list.addMessage(std::move(outMsg1), false);
+
+    EXPECT_EQ(list.getIncomingMessages().size(), expectedInSize);
+    EXPECT_EQ(list.getOutgoingMessages().size(), expectedOutSize);
+}
+
+// Tests the general case where several messages are added to each message list.
+TEST(PhybotMessageListTest, AddsSeveralMessages) {
+    CPhybotMessageList list;
+    size_t expectedInSize = 4, expectedOutSize = 2;
 
     // Random timestamp values for the messages
     uint32_t t1 = 1, t2 = 73, t3 = 56, t4 = 21, t5 = 19, t6 = 1000;
@@ -45,85 +66,84 @@ TEST(PhybotMessageListTest, MessageAddition) {
     std::unique_ptr<CPhybotMessage> outMsg2 = std::make_unique<CPhybotHeavyMessage>();
     initMsg(*outMsg2, t6);
 
-    // 1 incoming and 1 outgoing message list test
-    list.addMessage(std::move(incMsg1), true);
-    list.addMessage(std::move(outMsg1), false);
-    
-    EXPECT_EQ(list.getIncomingMessages().size(), expectedInSize1);
-    EXPECT_EQ(list.getOutgoingMessages().size(), expectedOutSize1);
-
     // 4 incoming and 2 outgoing message list test
+    list.addMessage(std::move(incMsg1), true);
     list.addMessage(std::move(incMsg2), true);
     list.addMessage(std::move(incMsg3), true);
     list.addMessage(std::move(incMsg4), true);
+    list.addMessage(std::move(outMsg1), false);
     list.addMessage(std::move(outMsg2), false);
 
-    EXPECT_EQ(list.getIncomingMessages().size(), expectedInSize2);
-    EXPECT_EQ(list.getOutgoingMessages().size(), expectedOutSize2);
+    EXPECT_EQ(list.getIncomingMessages().size(), expectedInSize);
+    EXPECT_EQ(list.getOutgoingMessages().size(), expectedOutSize);
 }
 
-// Tests the removal of older messages from the list
-TEST(PhybotMessageListTest, MessageRemoval) {
-    // Testing a case where H is bigger than the current timestamp. no message should be removed.
-    CPhybotMessageList bigHList;
-    size_t expcBigHInSize = 2, expcBigHOutSize = 2;
+// Testing a case where H is bigger than the current timestamp. no message should be removed.
+TEST(PhybotMessageListTest, KeepsMessagesWhenHIsLarge) {
+    CPhybotMessageList list;
+    size_t expcInSize = 2, expcOutSize = 2;
 
     // timestamp values for the messages
     uint32_t t1 = 1, t2 = 2, t3 = 3, t4 = 4;
-    uint32_t bigHTimestamp = 4;
-    uint32_t bigH = 1000;
+    uint32_t timestamp = 4;
+    uint32_t H = 1000;
 
-    std::unique_ptr<CPhybotMessage> incBigHMsg1 = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*incBigHMsg1, t1);
-    std::unique_ptr<CPhybotMessage> incBigHMsg2 = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*incBigHMsg2, t2);
-    std::unique_ptr<CPhybotMessage> outBigHMsg1 = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*outBigHMsg1, t3);
-    std::unique_ptr<CPhybotMessage> outBigHMsg2 = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*outBigHMsg2, t4);
+    std::unique_ptr<CPhybotMessage> incMsg1 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*incMsg1, t1);
+    std::unique_ptr<CPhybotMessage> incMsg2 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*incMsg2, t2);
+    std::unique_ptr<CPhybotMessage> outMsg1 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*outMsg1, t3);
+    std::unique_ptr<CPhybotMessage> outMsg2 = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*outMsg2, t4);
 
     // Add messages to the list
-    bigHList.addMessage(std::move(incBigHMsg1), true);
-    bigHList.addMessage(std::move(incBigHMsg2), true);
-    bigHList.addMessage(std::move(outBigHMsg1), false);
-    bigHList.addMessage(std::move(outBigHMsg2), false);
+    list.addMessage(std::move(incMsg1), true);
+    list.addMessage(std::move(incMsg2), true);
+    list.addMessage(std::move(outMsg1), false);
+    list.addMessage(std::move(outMsg2), false);
     
-    bigHList.removeOldMessages(bigHTimestamp, bigH);
+    list.removeOldMessages(timestamp, H);
 
-    EXPECT_EQ(bigHList.getIncomingMessages().size(), expcBigHInSize);
-    EXPECT_EQ(bigHList.getOutgoingMessages().size(), expcBigHOutSize);
+    EXPECT_EQ(list.getIncomingMessages().size(), expcInSize);
+    EXPECT_EQ(list.getOutgoingMessages().size(), expcOutSize);
+}
 
-    // Testing the basic case where the list has 1 message in each list and both should be removed.
-    CPhybotMessageList basicList;
+// Testing the basic case where the list has 1 message in each list and both should be removed.
+TEST(PhybotMessageListTest, RemovesExpiredMessage) {
+    CPhybotMessageList list;
 
     // timestamp values for the messages
-    uint32_t t5 = 5, t6 = 8;
-    uint32_t basicTimestamp = 20;
-    uint32_t basicH = 10;
+    uint32_t t1 = 5, t2 = 8;
+    uint32_t timestamp = 20;
+    uint32_t H = 10;
 
-    std::unique_ptr<CPhybotMessage> incBasicMsg = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*incBasicMsg, t5);
-    std::unique_ptr<CPhybotMessage> outBasicMsg = std::make_unique<CPhybotHeavyMessage>();
-    initMsg(*outBasicMsg, t6);
+    std::unique_ptr<CPhybotMessage> incMsg = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*incMsg, t1);
+    std::unique_ptr<CPhybotMessage> outMsg = std::make_unique<CPhybotHeavyMessage>();
+    initMsg(*outMsg, t2);
 
     // Add messages to the list
-    basicList.addMessage(std::move(incBasicMsg), true);
-    basicList.addMessage(std::move(outBasicMsg), false);
+    list.addMessage(std::move(incMsg), true);
+    list.addMessage(std::move(outMsg), false);
 
-    basicList.removeOldMessages(basicTimestamp, basicH);
+    list.removeOldMessages(timestamp, H);
 
     // Both lists should be empty after removal
-    EXPECT_EQ(basicList.getIncomingMessages().size(), 0);
-    EXPECT_EQ(basicList.getOutgoingMessages().size(), 0);
+    EXPECT_EQ(list.getIncomingMessages().size(), 0);
+    EXPECT_EQ(list.getOutgoingMessages().size(), 0);
+}
 
-    // Testing a general case where H is smaller than the current timestamp. (older messages should be removed)
-    CPhybotMessageList normalList;
+// Testing a general case where H is smaller than the current timestamp. (older messages should be removed)
+TEST(PhybotMessageListTest, RemovesExpiredMessages) {
+    
+    CPhybotMessageList list;
     size_t expcInSize = 2, expcOutSize = 2;
 
     // timestamp values for the messages
     uint32_t tStay1 = 5, tStay2 = 7, tRemove1 = 0, tRemove2 = 3;
-    uint32_t normalTimestamp = 8;
-    uint32_t normalH = 4;
+    uint32_t timestamp = 8;
+    uint32_t H = 4;
 
     std::unique_ptr<CPhybotMessage> incMsg1 = std::make_unique<CPhybotHeavyMessage>();
     initMsg(*incMsg1, tRemove1);
@@ -141,19 +161,19 @@ TEST(PhybotMessageListTest, MessageRemoval) {
     initMsg(*outMsg3, tStay2);
 
     // Add messages to the list
-    normalList.addMessage(std::move(incMsg1), true);
-    normalList.addMessage(std::move(incMsg2), true);
-    normalList.addMessage(std::move(incMsg3), true);
-    normalList.addMessage(std::move(incMsg4), true);
-    normalList.addMessage(std::move(outMsg1), false);
-    normalList.addMessage(std::move(outMsg2), false);
-    normalList.addMessage(std::move(outMsg3), false);
+    list.addMessage(std::move(incMsg1), true);
+    list.addMessage(std::move(incMsg2), true);
+    list.addMessage(std::move(incMsg3), true);
+    list.addMessage(std::move(incMsg4), true);
+    list.addMessage(std::move(outMsg1), false);
+    list.addMessage(std::move(outMsg2), false);
+    list.addMessage(std::move(outMsg3), false);
 
     // Incoming messages 1 and 2, and outgoing message 1 should be removed.
-    normalList.removeOldMessages(normalTimestamp, normalH);
+    list.removeOldMessages(timestamp, H);
 
-    EXPECT_EQ(normalList.getIncomingMessages().size(), expcInSize);
-    EXPECT_EQ(normalList.getOutgoingMessages().size(), expcOutSize);
+    EXPECT_EQ(list.getIncomingMessages().size(), expcInSize);
+    EXPECT_EQ(list.getOutgoingMessages().size(), expcOutSize);
 }
 } // namespace
 } // namespace argos
